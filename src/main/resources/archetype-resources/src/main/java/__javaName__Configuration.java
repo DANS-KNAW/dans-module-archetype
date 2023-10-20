e34f91478723224ae40c817c7335b91ce418d73e
Jan van Mansum <janvanmansum@gmail.com>
2023-10-20 12:44:45 +0200

    Fixed import of Configuration class

--- src/main/resources/archetype-resources/src/main/java/__javaName__Configuration.java
+++ src/main/resources/archetype-resources/src/main/java/__javaName__Configuration.java
@@ -5,7 +5,7 @@
 package ${package};
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.dropwizard.Configuration;
+import io.dropwizard.core.Configuration;
 
 import javax.validation.constraints.NotEmpty;
 
